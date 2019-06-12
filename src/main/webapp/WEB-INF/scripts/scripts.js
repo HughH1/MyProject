5d4da6b592cde9b3b6f994958cf013a94a78735a
HughH1 <hughfharding@gmail.com>
2019-06-12 18:05:12 +0100

    Added view tricks

---
+++ src/main/webapp/WEB-INF/scripts/scripts.js
@@ -0,0 +1,70 @@
+
+function fetchData(id, url) {
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.onreadystatechange = () => {
+            if (req.readyState === 4) {
+                if (req.status === 200) {
+                    resolve(req);
+                }
+                else {
+                    reject("Something went wrong");
+                }
+            }
+        }
+        req.open("GET", url+"/"+id);
+        console.log(id);
+        req.send();
+    });
+}
+
+let myVal = document.getElementById("theInput");
+
+
+function search() {
+    let value = myVal.value;
+    fetchData(value, "http://localhost:8080/SkateTricks-1.0/api/viewTrick").then((req) => {
+        let tricks = JSON.parse(req.responseText);
+        document.getElementById("myOutput").innerHTML = tricks.difficulty;
+        console.log(tricks);
+    });
+}
+
+
+
+
+// function sendReq() {
+//     fetchData("localhost:8080/SkateTricks-1.0/api/viewTrick/").then((req) => {
+//         let tricks = JSON.parse(req.responseText);
+//         console.log(tricks);
+
+
+//     });
+// }
+
+
+
+
+
+
+
+
+
+
+// function sendReq() {
+//     const req = new XMLHttpRequest();
+//     req.onreadystatechange = function myRequest() {
+//         if (req.readyState === 4) {
+//             let superHeroObject = JSON.parse(req.responseText);
+//             for (let x in superHeroObject) {
+//                 let paste = document.createElement("th");
+//                 paste.innerText = x;
+//                 document.getElementById("heading").appendChild(paste);
+//               
+//             }
+//         }
+//     }
+//     req.open("GET", "https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json");
+//     req.send();
+
+// }
