dce14d24c406ae9448f47fd53e325e3fe0b33e6e
HughH1 <hughfharding@gmail.com>
2019-06-13 13:13:45 +0100

    test change

--- src/main/webapp/scripts/scripts.js
+++ src/main/webapp/scripts/scripts.js
@@ -1,5 +1,5 @@
 
-function fetchData(id, url) {
+function fetchData(type, id, url) {
     return new Promise((resolve, reject) => {
         const req = new XMLHttpRequest();
         req.onreadystatechange = () => {
@@ -12,7 +12,7 @@ function fetchData(id, url) {
                 }
             }
         }
-        req.open("GET", url + "/" + id);
+        req.open(type, url + "/" + id);
         req.send();
     });
 }
@@ -22,70 +22,24 @@ let myVal = document.getElementById("theInput");
 
 function search() {
     let value = myVal.value;
-    fetchData(value, "http://35.234.153.117:8080/SkateTricks-1.0/api/viewTrick").then((req) => {
+    fetchData("GET", value, "http://35.234.153.117:8080/SkateTricks-1.0/api/viewTrick").then((req) => {
         let tricks = JSON.parse(req.responseText);
         document.getElementById("trickName").innerHTML = tricks.trick;
+        document.getElementById("trickDesc").innerHTML = tricks.desc;
+        document.getElementById("trickProgress").innerHTML = tricks.progress;
+        document.getElementById("trickDifficulty").innerHTML = tricks.difficulty;
         console.log(tricks);
     });
 }
 
-
-
-let userInput = document.getElementById("")
+let cVal = document.getElementById("cUserInput");
+cVal += document.getElementById("cUserInput2");
 
 function createTrick() {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
         const req = new XMLHttpRequest();
         req.open("POST", "http://35.234.153.117:8080/SkateTricks-1.0/api/createTrick");
         req.setRequestHeader("Content-Type", "application/json");
-
-
-
+        req.send(JSON.stringify(cVal));
     });
-    
-    
-    //function addAccount() {
-    // let request = new XMLHttpRequest();
-    // request.open("POST", "/JavaEEServer-1.0/api/accounts");
-    // request.setRequestHeader("Content-Type", "application/json")
-    // let body = {name: "Gaza"};
-    // request.send(JSON.stringify(body));
-    
-    
-
-
-
 }
-
-
-
-
-
-
-// function sendReq() {
-//     fetchData("localhost:8080/SkateTricks-1.0/api/viewTrick/").then((req) => {
-//         let tricks = JSON.parse(req.responseText);
-//         console.log(tricks);
-
-
-//     });
-// }
-
-
-// function sendReq() {
-//     const req = new XMLHttpRequest();
-//     req.onreadystatechange = function myRequest() {
-//         if (req.readyState === 4) {
-//             let superHeroObject = JSON.parse(req.responseText);
-//             for (let x in superHeroObject) {
-//                 let paste = document.createElement("th");
-//                 paste.innerText = x;
-//                 document.getElementById("heading").appendChild(paste);
-//               
-//             }
-//         }
-//     }
-//     req.open("GET", "https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json");
-//     req.send();
-
-// }
