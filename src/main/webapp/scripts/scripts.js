bc3f1ef27a0ede61fa7b66f928dd5d9ca7c65711
HughH1 <hughfharding@gmail.com>
2019-06-14 13:59:53 +0100

    streamlined code

--- src/main/webapp/scripts/scripts.js
+++ src/main/webapp/scripts/scripts.js
@@ -67,49 +67,58 @@ function createTrick() {
 function updateTrick() {
     let id = myVal.value;
 
-    let updatedTrick = {};
+    // let updatedTrick = {};
 
     let currentTrick = document.getElementById("trickName").innerText;
     let currentDesc = (document.getElementById("trickDesc")).innerText;
     let currentProgress = (document.getElementById("trickProgress")).innerText;
     let currentDifficulty = (document.getElementById("trickDifficulty")).innerText;
 
+    document.getElementById("uTrickName").value = document.getElementById("trickName").innerText;
+    document.getElementById("uDesc").value = document.getElementById("trickDesc").innerText;
+    document.getElementById("uProgress").value = document.getElementById("trickProgress").innerText;
+    document.getElementById("uDifficulty").value = document.getElementById("trickDifficulty").innerText;
+
+
     let newTrick = document.getElementById("uTrickName").value;
     let newDesc = document.getElementById("uDesc").value;
     let newProgress = document.getElementById("uProgress").value;
     let newDifficulty = document.getElementById("uDifficulty").value;
 
-    if (newTrick != currentTrick && newTrick != "" && newTrick != undefined) {
-        updatedTrick.trick = newTrick;
-        console.log(updatedTrick);
-    } else {
-        updatedTrick.trick = currentTrick;
-        console.log(updatedTrick);
-    }
 
-    if (newDesc != currentDesc && newDesc != "" && newDesc != undefined) {
-        updatedTrick.desc = newDesc;
-        console.log(updatedTrick);
-    } else {
-        updatedTrick.desc = currentDesc;
-        console.log(updatedTrick);
-    }
+    const updatedTrick = {
+        trick: (newTrick && newTrick != currentTrick) ? newTrick : currentTrick,
+        desc: (newDesc && newDesc != currentDesc) ? newDesc : currentDesc,
+        progress: (newProgress && newProgress != currentProgress) ? newProgress : currentProgress,
+        difficulty: (newDifficulty && newDifficulty != currentDifficulty) ? newDifficulty : currentDifficulty,
 
-    if (newProgress != currentProgress && newProgress != "" && newProgress != undefined) {
-        updatedTrick.progress = newProgress
-        console.log(updatedTrick);
-    } else {
-        updatedTrick.progress = currentProgress
-        console.log(updatedTrick);
-    }
+    };
 
-    if (newDifficulty != currentDifficulty && newDifficulty != "" && newDifficulty != undefined) {
-        updatedTrick.difficulty = newDifficulty;
-        console.log(updatedTrick);
-    } else {
-        updatedTrick.difficulty = currentDifficulty;
-        console.log(updatedTrick);
-    }
+    // updatedTrick.desc = (newDesc && newDesc != currentDesc) ? newDesc : currentDesc;
+
+    // if (newDesc && newDesc != currentDesc) {
+    //     updatedTrick.desc = newDesc;
+
+    // } else {
+    //     updatedTrick.desc = currentDesc;
+
+    // }
+
+    // if (newProgress != currentProgress && newProgress != "" && newProgress != undefined) {
+    //     updatedTrick.progress = newProgress
+
+    // } else {
+    //     updatedTrick.progress = currentProgress;
+
+    // }
+
+    // if (newDifficulty != currentDifficulty && newDifficulty != "" && newDifficulty != undefined) {
+    //     updatedTrick.difficulty = newDifficulty;
+
+    // } else {
+    //     updatedTrick.difficulty = currentDifficulty;
+    //     console.log(updatedTrick);
+    // }
 
     console.log(updatedTrick);
 
@@ -121,10 +130,10 @@ function updateTrick() {
 }
 
 
-function deleteTrick(){
+function deleteTrick() {
     let id = myVal.value;
-    checkServer("DELETE", id, "http://35.234.153.117:8080/SkateTricks-1.0/api/delete/" ).then((req) => {
-       req.setRequestHeader("Content-Type", "application/json");
-       req.send();
+    checkServer("DELETE", id, "http://35.234.153.117:8080/SkateTricks-1.0/api/delete/").then((req) => {
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send();
     });
 }
