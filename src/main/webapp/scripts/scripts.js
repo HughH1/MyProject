227123c672a080e3798d01a3065e91a3fec1495d
Hugh Harding <hughfharding@gmail.com>
2019-06-14 22:39:55 +0100

    fix

--- src/main/webapp/scripts/scripts.js
+++ src/main/webapp/scripts/scripts.js
@@ -112,6 +112,6 @@ function deleteTrick() {
     checkServer("DELETE", trickId.id, "http://35.234.153.117:8080/SkateTricks-1.0/api/delete").then((req) => {
         console.log("Trick deleted!");
         let theVal = "Trick " + trickId.id + "has been deleted";
-        document.getElementById("deleteConfirmed").innerHTML.value = theVal;
+        document.getElementById("deleteConfirmed").value = theVal;
     });
 }
