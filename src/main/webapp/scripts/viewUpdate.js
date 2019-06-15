8cd68d9875acbbd7fc25011a53e98c1260471810
Hugh Harding <hughfharding@gmail.com>
2019-06-15 16:06:24 +0100

    error log

--- src/main/webapp/scripts/viewUpdate.js
+++ src/main/webapp/scripts/viewUpdate.js
@@ -4,17 +4,21 @@ function searchUpdate() {
         let tricks = JSON.parse(req.responseText);
         tricks = tricks[0]
         sessionStorage.setItem("tricks", req.responseText);
-   
-            // document.getElementById("trickName").innerHTML = tricks.trick;
-            // document.getElementById("trickDesc").innerHTML = tricks.desc;
-            // document.getElementById("trickProgress").innerHTML = tricks.progress;
-            // document.getElementById("trickDifficulty").innerHTML = tricks.difficulty;
-     
-            document.getElementById("uTrickName").value = tricks.trick;
-            document.getElementById("uTrickDesc").value = tricks.desc;
-            document.getElementById("uTrickProgress").value = tricks.progress;
-            document.getElementById("uTrickDifficulty").value = tricks.difficulty;
-        
+
+        // document.getElementById("trickName").innerHTML = tricks.trick;
+        // document.getElementById("trickDesc").innerHTML = tricks.desc;
+        // document.getElementById("trickProgress").innerHTML = tricks.progress;
+        // document.getElementById("trickDifficulty").innerHTML = tricks.difficulty;
+
+        console.log(tricks);
+        console.log(tricks.desc);
+        console.log(tricks.progress);
+        console.log(tricks.difficulty);
+        document.getElementById("uTrickName").value = tricks.trick;
+        document.getElementById("uTrickDesc").value = tricks.desc;
+        document.getElementById("uTrickProgress").value = tricks.progress;
+        document.getElementById("uTrickDifficulty").value = tricks.difficulty;
+
 
 
 
