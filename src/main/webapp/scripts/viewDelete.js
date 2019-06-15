9c514110470d3a437e1a4ad695facd9ec76465b2
Hugh Harding <hughfharding@gmail.com>
2019-06-15 16:49:20 +0100

    delete trick user confimation of which trick

--- src/main/webapp/scripts/viewDelete.js
+++ src/main/webapp/scripts/viewDelete.js
@@ -4,19 +4,11 @@ function search() {
         let tricks = JSON.parse(req.responseText);
         tricks = tricks[0]
         sessionStorage.setItem("tricks", req.responseText);
-   
-            // document.getElementById("trickName").innerHTML = tricks.trick;
-            // document.getElementById("trickDesc").innerHTML = tricks.desc;
-            // document.getElementById("trickProgress").innerHTML = tricks.progress;
-            // document.getElementById("trickDifficulty").innerHTML = tricks.difficulty;
+    
+            document.getElementById("trickName").innerHTML = tricks.trick;
+            document.getElementById("trickDesc").innerHTML = tricks.desc;
+            document.getElementById("trickProgress").innerHTML = tricks.progress;
+            document.getElementById("trickDifficulty").innerHTML = tricks.difficulty;
      
-            // document.getElementById("uTrickName").value = tricks.trick;
-            // document.getElementById("uTrickDesc").value = tricks.desc;
-            // document.getElementById("uTrickProgress").value = tricks.progress;
-            // document.getElementById("uTrickDifficulty").value = tricks.difficulty;
-        
-
-
-
     });
 }
