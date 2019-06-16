1508f527f33b87c8152e16eac55a11adfe02aacd
Hugh Harding <hughfharding@gmail.com>
2019-06-16 19:50:20 +0100

    fix

--- src/main/webapp/scripts/scripts.js
+++ src/main/webapp/scripts/scripts.js
@@ -16,9 +16,12 @@ function checkServer(type, id, url, object) {
             req.open(type, url);
             req.setRequestHeader("Content-Type", "application/json");
             req.send(object);
+        } else if (type === "DELETE") {
+            req.open(type, url + "/" + id);
+            req.setRequestHeader("Content-Type", "application/json");
+            req.send();
         }
         else {
-            console.log(object);
             req.open(type, url + "/" + id);
             req.setRequestHeader("Content-Type", "application/json");
             req.send(object);
@@ -27,74 +30,3 @@ function checkServer(type, id, url, object) {
 }
 
 let myVal = document.getElementById("theInput");
-
-function search() {
-    let value = myVal.value;
-    checkServer("GET", value, "http://35.234.153.117:8080/SkateTricks-1.0/api/viewTrick").then((req) => {
-        let tricks = JSON.parse(req.responseText);
-        document.getElementById("trickName").innerHTML = tricks.trick;
-        document.getElementById("trickDesc").innerHTML = tricks.desc;
-        document.getElementById("trickProgress").innerHTML = tricks.progress;
-        document.getElementById("trickDifficulty").innerHTML = tricks.difficulty;
-
-        document.getElementById("uTrickName").value = tricks.trick;
-        document.getElementById("uDesc").value = tricks.desc;
-        document.getElementById("uProgress").value = tricks.progress;
-        document.getElementById("uDifficulty").value = tricks.difficulty;
-    }).catch(error => {
-        console.log(error);
-    });
-}
-
-
-function createTrick() {
-    let createdTrick = {};
-
-    createdTrick
-        .trick = document.getElementById("cTrickName").value;
-    createdTrick
-        .desc = document.getElementById("cDesc").value;
-    createdTrick
-        .progress = document.getElementById("cProgress").value;
-    createdTrick
-        .difficulty = document.getElementById("cDifficulty").value;
-
-    JSON.stringify(createdTrick);
-
-    checkServer("POST", null, "http://35.234.153.117:8080/SkateTricks-1.0/api/createTrick", JSON.stringify(createdTrick)).then((req) => {
-
-        console.log(createdTrick
-        );
-    });
-}
-
-function updateTrick() {
-    let id = myVal.value;
-
-    let currentTrick = document.getElementById("trickName").innerText;
-    let currentDesc = (document.getElementById("trickDesc")).innerText;
-    let currentProgress = (document.getElementById("trickProgress")).innerText;
-    let currentDifficulty = (document.getElementById("trickDifficulty")).innerText;
-
-    let newTrick = document.getElementById("uTrickName").value;
-    let newDesc = document.getElementById("uDesc").value;
-    let newProgress = document.getElementById("uProgress").value;
-    let newDifficulty = document.getElementById("uDifficulty").value;
-
-    const updatedTrick = {
-        trick: (newTrick && newTrick != currentTrick) ? newTrick : currentTrick,
-        desc: (newDesc && newDesc != currentDesc) ? newDesc : currentDesc,
-        progress: (newProgress && newProgress != currentProgress) ? newProgress : currentProgress,
-        difficulty: (newDifficulty && newDifficulty != currentDifficulty) ? newDifficulty : currentDifficulty,
-    };
-
-    console.log(updatedTrick);
-
-    checkServer("PUT", id, "http://35.234.153.117:8080/SkateTricks-1.0/api/updateTrick",
-        JSON.stringify(updatedTrick)).then((req) => {
-            console.log("updated");
-        });
-
-        search();
-
-   }
